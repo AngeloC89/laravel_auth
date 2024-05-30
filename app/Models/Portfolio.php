69b20d69fc3bebb498ee819f42d9400c47bd3781
AngeloC89 <Angelociulla89@gmail.com>
2024-05-30 18:42:43 +0200

    data loaded into the database

--- app/Models/Portfolio.php
+++ app/Models/Portfolio.php
@@ -4,9 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Portfolio extends Model
 {
     use HasFactory;
     protected $fillable = ['title', 'content', 'status', 'slug'];
+
+    public static function generateSlug($title){
+        return Str::slug($title, '-');
+    }
 }
--- database/seeders/PortfolioSeeder.php
+++ database/seeders/PortfolioSeeder.php
@@ -4,14 +4,25 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker; 
+use App\Models\Portfolio;
 
 class PortfolioSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for($i = 0; $i < 10; $i++) {
+
+            $new_portfolio = new Portfolio();
+            $new_portfolio->title = $faker->sentence();
+            $new_portfolio->content = $faker->text();
+            $new_portfolio->status = $faker->word();
+            $new_portfolio->slug = Portfolio::generateSlug($new_portfolio->title);          
+            $new_portfolio->save();
+        }
     }
+
 }
