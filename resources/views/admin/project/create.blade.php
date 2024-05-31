ff8bdfe35e6e161322fcb0ed8b59cb381b8963e3
AngeloC89 <Angelociulla89@gmail.com>
2024-05-31 23:06:11 +0200

    fixed form for create and edit

--- resources/views/admin/project/create.blade.php
+++ resources/views/admin/project/create.blade.php
@@ -4,61 +4,28 @@
 <form class="p-4" action="{{ route('admin.project.store') }}" method="POST">
         @csrf
         <div class="mb-3">
-            <label for="title" class="form-label text-white " >Titolo</label>
+            <label for="title" class="form-label " >Title</label>
             <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{old('title')}}" required> 
             @error('title')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </div>
         <div class="mb-3">
-            <label for="description" class="form-label text-white">Descrizione</label>
-            <textarea type="text" class="form-control @error('title') is-invalid @enderror" id="description" name="description" rows="3" >{{old('description')}}</textarea>
+            <label for="content" class="form-label">Content</label>
+            <textarea type="text" class="form-control @error('title') is-invalid @enderror" id="content" name="content" rows="3" >{{old('content')}}</textarea>
             
         </div>
+     
         <div class="mb-3">
-            <label for="thumb" class="form-label text-white">Immagine</label>
-            <input type="text" class="form-control @error('title') is-invalid @enderror" id="thumb" name="thumb"  value="{{old('thumb')}}" required>
-         
-            @error('thumb')
-                <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-        </div>
-        <div class="mb-3">
-            <label for="price" class="form-label text-white">Prezzo</label>
-            <input type="text" class="form-control @error('price') is-invalid @enderror" id="price" name="price"  value="{{old('price')}}" required>
-           
-            @error('price')
-                <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-        </div>
-        <div class="mb-3">
-            <label for="series" class="form-label text-white ">Serie</label>
-            <input type="text" class="form-control @error('series') is-invalid @enderror" id="series" name="series" value="{{old('series')}}" required>
+            <label for="status" class="form-label">Status</label>
+            <input type="text" class="form-control @error('status') is-invalid @enderror" id="status" name="status" value="{{old('status')}}" required>
        
-            @error('series')
+            @error('status')
                 <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </div>
-        <div class="mb-3">
-            <label for="sale_date" class="form-label text-white">Data di vendita</label>
-            <input type="text" class="form-control @error('sale_date') is-invalid @enderror" id="sale_date" name="sale_date" value=" {{old('sale_date')}}" required>
-           
-            @error('sale_date')
-                <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-        </div>
-        <div class="mb-3">
-            <label for="type" class="form-label text-white">Tipo</label>
-            <select type="text" name="type" id="type" class="form-control @error('title') is-invalid @enderror" value="     {{old('type')}}">
-                <option value="project book">project book</option>
-                <option value="graphic book">Graphic book</option>
-                <option value="grafic novel">Graphic novel</option>
-            </select>
-       
-            @error('type')
-                <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-        </div>   
+      
+      
         <button class="btn btn-primary" type="submit">Crea</button>
     </form>
 
--- resources/views/admin/project/edit.blade.php
+++ resources/views/admin/project/edit.blade.php
@@ -2,9 +2,36 @@
 @section('content')
 
 
-<div class="content">
-    <div class="container">
-     <h1>Edit</h1>
-    </div>
-</div>
+<
+<section class="container p-5">
+
+    <form action="{{ route('admin.project.update', $project->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+        <div class="mb-3">
+            <label for="title" class="form-label @error('title') is-invalid @enderror">Title</label>
+            <input type="text" class="form-control" id="title" name="title" placeholder="Titolo" value="{{ $project->title }}" >
+        
+            @error('title')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label for="content" class="form-label">Content</label>
+            <input type="text" class="form-control @error('content') is-invalid @enderror" id="description" name="content" placeholder="Descrizone" value="{{$project->content }}">  
+        </div>
+      
+        
+        <div class="mb-3">
+            <label for="status" class="form-label">Status</label>
+            <input type="text" class="form-control @error('status') is-invalid @enderror" id="status" name="status" placeholder="Serie" value="{{ $project->status }}" >
+   
+            @error('status')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
+        
+        <button class="btn btn-primary" type="submit">Modifica</button>
+    </form>
+</section>
 @endsection
